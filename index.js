be51a5d536e86454cf373907f49f63f7c7347b9c
Levi Miller <paullevimiller@gmail.com>
2019-09-19 15:18:46 -0700

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string.slice(0, 1).toUpperCase() + string.slice(1, string.length);
+  };
+
+  static sanitize(string) {
+    const sanitizedString = string.replace(/[^A-Za-z0-9 '-]/g, '');
+    return sanitizedString;
+  };
+
+  static titleize(string) {
+    const exceptions = ["the", "a", "an", "but", "of", "and", "of", "for", "at", "by", "from"];
+    const titledString = string.slice(0, 1).toUpperCase() + string.slice(1, string.length);
+    const strArr = titledString.split(" ");
+    const titleStrArr = strArr.map(str => {
+      if (exceptions.includes(str)) {
+        return str;
+      } else {
+        return str.slice(0, 1).toUpperCase() + str.slice(1, str.length);
+      };
+    });
+    const titleStr = titleStrArr.join(" ")
+    return titleStr;
+  };
+
 }
